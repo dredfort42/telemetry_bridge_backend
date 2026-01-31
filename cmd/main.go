f2fe28281781202239375265187ecd1c1fa576f3
Dmitry Novikov <dredfort.42@gmail.com>
2026-01-31 17:22:56 +0000

    udp broadcast

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"net"
+	"os"
+	"time"
+)
+
+const broadcastPort = 9999
+const operationPort = 8888
+
+type ServerInfo struct {
+	Type string `json:"type"`
+	IP   string `json:"ip"`
+	Port int    `json:"port"`
+}
+
+type Broadcaster struct {
+	Addr *net.UDPAddr
+	Info ServerInfo
+}
+
+func (b *Broadcaster) Start(interval time.Duration) error {
+	conn, err := net.DialUDP("udp", nil, b.Addr)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	data, _ := json.Marshal(b.Info)
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		conn.Write(data)
+	}
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	// Get server IP (example: first non-loopback)
+	addrs, _ := net.InterfaceAddrs()
+	var serverIP string
+	for _, addr := range addrs {
+		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil {
+			serverIP = ipnet.IP.String()
+			break
+		}
+	}
+
+	info := ServerInfo{
+		Type: "TelemetryBridge",
+		IP:   serverIP,
+		Port: operationPort,
+	}
+
+	addr := &net.UDPAddr{IP: net.ParseIP(info.IP), Port: broadcastPort}
+	b := Broadcaster{Addr: addr, Info: info}
+	if err := b.Start(2 * time.Second); err != nil {
+		os.Exit(1)
+	}
 }
