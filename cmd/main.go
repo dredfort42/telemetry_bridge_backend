f54c08cd25a87924c78d405309855928548600fc
Dmitry Novikov <dredfort.42@gmail.com>
2026-02-01 18:22:46 +0000

    simple device registration

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"net"
 	"os"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 const serverType = "TelemetryBridge"
@@ -60,7 +64,26 @@ func main() {
 
 	addr := &net.UDPAddr{IP: net.ParseIP(broadcastNet), Port: broadcastPort}
 	b := Broadcaster{Addr: addr, Info: info}
-	if err := b.Start(2 * time.Second); err != nil {
+	go func() {
+		if err := b.Start(2 * time.Second); err != nil {
+			os.Exit(1)
+		}
+	}()
+
+	router := gin.Default()
+	router.POST("/register", func(c *gin.Context) {
+		body := make(map[string]any)
+		if err := c.BindJSON(&body); err != nil {
+			c.JSON(400, gin.H{"error": "invalid JSON"})
+			return
+		}
+
+		log.Printf("Received registration: %v", body)
+
+		c.JSON(200, gin.H{"status": "registered", "data": body})
+	})
+
+	if err := router.Run(fmt.Sprintf(":%d", operationPort)); err != nil {
 		os.Exit(1)
 	}
 }
