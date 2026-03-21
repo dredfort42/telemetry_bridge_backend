7a07f2424e423251f5f6e6fd7996589b42bac42b
Dmitry Novikov <dredfort.42@gmail.com>
2026-03-21 20:19:52 +0000

    config ready

--- cmd/telemetry_bridge/main.go
+++ cmd/telemetry_bridge/main.go
@@ -1,53 +1,123 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
-	"log"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 	"telemetry_bridge/internal/config"
 
-	"github.com/gin-gonic/gin"
+	log "github.com/dredfort42/go_logger"
+)
+
+// Version is set at build time using -ldflags. Default is "0.0.0" if not provided.
+var Version = "0.0.0"
+
+// Application constants.
+const (
+	AppName     = "SENSOTECH.net Telemetry Bridge"
+	Environment = "development"
 )
 
 func main() {
-	config, err := config.Init()
-	if err != nil {
-		log.Fatalf("Failed to load config: %v", err)
+	var (
+		versionFlag = flag.Bool("version", false, "Show version information")
+		helpFlag    = flag.Bool("help", false, "Show help information")
+		configFlag  = flag.String("config", "", "Path to config file")
+	)
+
+	flag.Parse()
+
+	if *versionFlag {
+		fmt.Printf("%s version %s\n", AppName, Version)
+		os.Exit(0)
 	}
+
+	if *helpFlag {
+		showUsage()
+		os.Exit(0)
+	}
+
+	log.Info.Printf("%s version %s is starting...", AppName, Version)
+	defer log.Info.Printf("%s version %s stopped successfully", AppName, Version)
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	config.App.AppName = AppName
+	config.App.Version = Version
+	config.App.Environment = Environment
+
+	if err := config.Init(ctx, *configFlag); err != nil {
+		log.Error.Println("Config initialization failed:", err)
+		showUsage()
+		cancel()
+	}
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+	}()
+
+	wg.Wait()
 
 	// if err := digest.New(config, )
 
-	router := gin.Default()
-	router.POST("/register", func(c *gin.Context) {
-		body := make(map[string]any)
-		if err := c.BindJSON(&body); err != nil {
-			c.JSON(400, gin.H{"error": "invalid JSON"})
-			return
-		}
+	// router := gin.Default()
+	// router.POST("/register", func(c *gin.Context) {
+	// 	body := make(map[string]any)
+	// 	if err := c.BindJSON(&body); err != nil {
+	// 		c.JSON(400, gin.H{"error": "invalid JSON"})
+	// 		return
+	// 	}
 
-		log.Printf("Received registration: %v", body)
+	// 	log.Printf("Received registration: %v", body)
 
-		c.JSON(200, gin.H{"status": "registered", "data": body})
-	})
+	// 	c.JSON(200, gin.H{"status": "registered", "data": body})
+	// })
 
-	router.POST("/data", func(c *gin.Context) {
-		// 	  json["mac"] = WiFi.macAddress();
-		// json["temperature_c"] = temperature;
-		// json["humidity_percent"] = humidity;
-		// json["timestamp"] = millis();
-		body := make(map[string]any)
-		if err := c.BindJSON(&body); err != nil {
-			c.JSON(400, gin.H{"error": "invalid JSON"})
-			return
-		}
+	// router.POST("/data", func(c *gin.Context) {
+	// 	// 	  json["mac"] = WiFi.macAddress();
+	// 	// json["temperature_c"] = temperature;
+	// 	// json["humidity_percent"] = humidity;
+	// 	// json["timestamp"] = millis();
+	// 	body := make(map[string]any)
+	// 	if err := c.BindJSON(&body); err != nil {
+	// 		c.JSON(400, gin.H{"error": "invalid JSON"})
+	// 		return
+	// 	}
 
-		log.Printf("Received data: %v", body)
-		// log.Printf("Timestamp: %v", int64(body["timestamp"].(float64)))
+	// 	log.Printf("Received data: %v", body)
+	// 	// log.Printf("Timestamp: %v", int64(body["timestamp"].(float64)))
 
-		c.JSON(200, gin.H{"status": "OK", "data": body})
-	})
+	// 	c.JSON(200, gin.H{"status": "OK", "data": body})
+	// })
 
-	if err := router.Run(fmt.Sprintf(":%s", config.Server.Port)); err != nil {
-		os.Exit(1)
-	}
+	// if err := router.Run(fmt.Sprintf(":%s", config.Server.Port)); err != nil {
+	// 	os.Exit(1)
+	// }
+}
+
+// showUsage displays the usage information for the application.
+func showUsage() {
+	fmt.Printf(`%s - A telemetry bridge for IoT devices
+
+Usage:
+  %s [flags]
+
+Flags:
+  --version      Show version information and exit
+  --help         Show this help message and exit
+  --config PATH  Specify path to config file.
+                Supports YAML, JSON and INI formats.
+
+Example:
+  %s --config /path/to/config.yaml
+
+`, AppName, AppName, AppName)
 }
--- internal/config/load.go
+++ internal/config/load.go
@@ -12,61 +12,29 @@ import (
 // ServiceConfig is a struct for streams server configuration
 type ServiceConfig struct {
 	Host           string
-	APIPort           int
-	DigestPort	  int
+	Port           int
 	ReadTimeout    time.Duration
 	WriteTimeout   time.Duration
 	IdleTimeout    time.Duration
 	MaxHeaderBytes int
 }
 
-// KafkaConfig is a struct for Kafka configuration
-type KafkaConfig struct {
-	Brokers           []string
-	TopicSystemDigest string
-	TopicSensorData   string
-	ConsumerConfig    map[string]any
-	ProducerConfig    map[string]any
-}
-
-// // RedisConfig is a struct for Redis configuration
-// type RedisConfig struct {
-// 	Host            string
-// 	Port            int
-// 	User            string
-// 	Password        string
-// 	DB              int
-// 	PoolSize        int
-// 	MinIdleConns    int
-// 	MaxConnAge      time.Duration
-// 	IdleTimeout     time.Duration
-// 	PoolTimeout     time.Duration
-// 	DialTimeout     time.Duration
-// 	ReadTimeout     time.Duration
-// 	WriteTimeout    time.Duration
-// 	MaxRetries      int
-// 	MaxRetryBackoff time.Duration
-// }
-
-type PublicURLs struct {
-	Register string
-	Data     string
+// DigesterConfig is a struct for digester configuration
+type DigesterConfig struct {
+	BroadcastInterval time.Duration
+	BroadcastAddress  string
+	BroadcastPort     int
 }
 
 // AppConfig represents complete application configuration
 type AppConfig struct {
-	UUID              string
-	AppName           string
-	Version           string
-	Environment       string
-	HeartbeatInterval time.Duration
-	JWTSecret         string
-	Debug             bool
-	Service           ServiceConfig
-	// Database          DatabaseConfig
-	Kafka KafkaConfig
-	// Redis             RedisConfig
-	PublicURLs PublicURLs
+	UUID        string
+	AppName     string
+	Version     string
+	Environment string
+	Debug       bool
+	Service     ServiceConfig
+	Digester    DigesterConfig
 }
 
 // App is the global application configuration
@@ -82,8 +50,7 @@ func Init(ctx context.Context, configPath string) error {
 
 	// Define comprehensive defaults
 	defaults := map[string]any{
-		"debug":              false,
-		"heartbeat_interval": 10 * time.Second,
+		"debug": false,
 
 		"service.host":             "localhost",
 		"service.port":             8080,
@@ -91,50 +58,30 @@ func Init(ctx context.Context, configPath string) error {
 		"service.write_timeout":    "30s",
 		"service.idle_timeout":     "120s",
 		"service.max_header_bytes": 1048576, // 1MB
+
+		"digester.broadcast_interval": "10s",
+		"digester.broadcast_address":  "255.255.255.255",
+		"digester.broadcast_port":     8888,
 	}
 
 	// Try to load from config file with defaults and validation
 	opts := &config.LoadOptions{
 		DefaultValues: defaults,
-		RequiredKeys: []string{
-			"jwt_secret",
-
-			"database.host",
-			"database.port",
-			"database.name",
-			"database.user",
-			"database.password",
-
-			"kafka.brokers",
-			"kafka.topic_system_digest",
-			"kafka.topic_sensor_data",
-
-			"redis.host",
-			"redis.port",
-			"redis.password",
-			"redis.db",
-		},
+		RequiredKeys:  []string{},
 		ValidationFunc: func(data map[string]any) error {
-			// Validate port range
+			// Validate service port range
 			if port, ok := data["service.port"].(int); ok {
 				if port < 1024 || port > 65535 {
 					return fmt.Errorf("service port must be between 1 and 65535, got %d", port)
 				}
 			}
 
-			// // Validate database port
-			// if dbPort, ok := data["database.port"].(int); ok {
-			// 	if dbPort < 1024 || dbPort > 65535 {
-			// 		return fmt.Errorf("database port must be between 1 and 65535, got %d", dbPort)
-			// 	}
-			// }
-
-			// // Validate Redis port
-			// if redisPort, ok := data["redis.port"].(int); ok {
-			// 	if redisPort < 1024 || redisPort > 65535 {
-			// 		return fmt.Errorf("redis port must be between 1 and 65535, got %d", redisPort)
-			// 	}
-			// }
+			// Validate digester port range
+			if port, ok := data["digester.broadcast_port"].(int); ok {
+				if port < 1024 || port > 65535 {
+					return fmt.Errorf("digester broadcast port must be between 1 and 65535, got %d", port)
+				}
+			}
 
 			return nil
 		},
